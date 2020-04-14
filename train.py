f3c61a2ca2095c087bc86ec950b02643a19d84f7
soohmatthew <matthewsoh18@gmail.com>
2020-04-14 17:22:58 +0800

    Added augmentations

--- train.py
+++ train.py
@@ -26,7 +26,7 @@ def main(args):
         transforms.ToPILImage(),
         transforms.Resize(args.crop_size),
         transforms.ToTensor(), 
-        #transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
         ])
     
     # Load vocabulary wrapper
