8b295ee59bd64278e9abe9972409ffb26e027f71
gorold <woogerald@yahoo.com.sg>
2020-04-25 17:05:44 +0000

    added batched sampling

--- model.py
+++ model.py
@@ -128,57 +128,44 @@ class DecoderRNN(nn.Module):
         logits = self.linear(lstm2_out)
         
         return logits, (han, can, hln, cln)        
-
-    def _internal_forward(self, X, sample=False):
-        """
-        Forward function used by both training and sampling methods. Not to be called directly
-        inputs:
-            X: Packed sequence of [input]
-            type: torch.nn.utils.rnn.PackedSequence
-
-            sample: boolean flag on whether to sample or not i.e. to test
-            type: bool
-
-        returns:
-            X: Predicted output sequence
-            type: tensor
-        """
-        X, self.hidden = self.lstm(X, self.hidden)
-        self.lstm.flatten_parameters() # Save memory
-
-        if not sample:
-            X, _ = pad_packed_sequence(X, padding_value=-1, batch_first=True) # Make sure to ignore -1 for cross-entropy function
-            X = X.contiguous()
-
-        X = X.view(-1, X.shape[2]) # Reshape to (batch_size * max_seq_length, hidden_size)
-        X = self.linear(X) # Output should be shape (batch_size * max_seq_length, # classes)
-
-        if sample:
-            X /= self.temperature
-            X = self.softmax(X)
-
-        return X
    
     def sample(self, features, states=None):
-        """Generate captions for given image features using greedy search."""
-        sampled_ids = []
-        inputs = features.unsqueeze(1)
-        for i in range(self.max_seg_length):
-            hiddens, states = self.lstm(inputs, states)          # hiddens: (batch_size, 1, hidden_size)
-            outputs = self.linear(hiddens.squeeze(1))            # outputs:  (batch_size, vocab_size)
-            _, predicted = outputs.max(1)                        # predicted: (batch_size)
-            sampled_ids.append(predicted)
-            inputs = self.embed(predicted)                       # inputs: (batch_size, embed_size)
-            inputs = inputs.unsqueeze(1)                         # inputs: (batch_size, 1, embed_size)
-        sampled_ids = torch.stack(sampled_ids, 1)                # sampled_ids: (batch_size, max_seq_length)
-        return sampled_ids
-    
-    def _get_softmax_probs(self, X, embed=True):
-        if embed:
-            X = self.embedding(X)
-            X = self.dropout(X)
-        X = self._internal_forward(X, sample=True)
-        return X
+        """
+        Generate captions for given image features using greedy search.
+        
+        Parameters
+        ----------
+        features : tensor
+            Output of EncoderCNN. Tensor of shape [B, C, H, W]
+            
+        Returns
+        -------
+        """
+        batch_size = features.shape[0]
+        lengths = [1 for _ in range(batch_size)]
+        outputs = torch.empty((batch_size, 0), dtype=torch.long).to(self.device)
+        done = torch.full((batch_size, 1), False, dtype=torch.bool).to(self.device)
+        ha0 = ca0 = hl0 = cl0 = None
+        prev_word = torch.full((batch_size, 1), self.vocab('<start>'), dtype=torch.long).to(self.device)
+        while not torch.all(done):
+            logits, (ha0, ca0, hl0, cl0) = self(features, prev_word, lengths, ha0, ca0, hl0, cl0)
+            prev_word = logits.argmax(dim=2)
+            outputs = torch.cat((outputs, prev_word), dim=1)
+            done[prev_word == self.vocab('<end>')] = True
+        return outputs
+            
+        
+#         sampled_ids = []
+#         inputs = features.unsqueeze(1)
+#         for i in range(self.max_seg_length):
+#             hiddens, states = self.lstm(inputs, states)          # hiddens: (batch_size, 1, hidden_size)
+#             outputs = self.linear(hiddens.squeeze(1))            # outputs:  (batch_size, vocab_size)
+#             _, predicted = outputs.max(1)                        # predicted: (batch_size)
+#             sampled_ids.append(predicted)
+#             inputs = self.embed(predicted)                       # inputs: (batch_size, embed_size)
+#             inputs = inputs.unsqueeze(1)                         # inputs: (batch_size, 1, embed_size)
+#         sampled_ids = torch.stack(sampled_ids, 1)                # sampled_ids: (batch_size, max_seq_length)
+#         return sampled_ids
     
     def beam_search(self, features, k=5):
         """
@@ -228,37 +215,4 @@ class DecoderRNN(nn.Module):
                 temp_candidates = new_temp_candidates[:k-len(candidates)]
             candidates.sort(key=lambda x: x[1])
         return candidates[-1]
-            
-
-    def sample_caption(self, first_input_tensor):
-        """
-        Generate sample sentence by feeding the output of the CNN into the LSTM continuously until the EOS is returned.
-        inputs:
-            X: Packed sequence of [input]
-            type: torch.nn.utils.rnn.PackedSequence
-
-            sample: boolean flag on whether to sample or not i.e. to test
-            type: bool
-
-        returns:
-            output_caption: Output Caption of Image
-            type: str
-        """
-        X = first_input_tensor.unsqueeze(0).to(self.device)
-        with torch.no_grad():  # no need to track history in sampling
-            self.hidden = self.init_hidden(batch_size = 1) # Initialise hidden and cell states
-            output_caption = ''
-
-            for idx in range(self.max_seg_length):
-                if idx == 1:
-                    X = self._get_softmax_probs(X, embed = True)
-                else:
-                    X = self._get_softmax_probs(X, embed = True)
-                topi = np.random.choice(a = X.shape[1], size = X.shape[0], p = X.squeeze().cpu().numpy())[0]
-                if topi == 0: # EOS Token
-                    break
-                else:
-                    word = self.vocab.idx2word[topi]
-                    output_caption += ' ' + word
-                X = torch.tensor([topi]).unsqueeze(0).to(self.device).type(torch.long)
-            return output_caption
+           
